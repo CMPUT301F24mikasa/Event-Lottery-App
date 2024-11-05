9a877062f0749eccc4c8abe85ae9732dc05f3d50
Sankalpa Paudel <163387708+srajpaudel@users.noreply.github.com>
2024-11-04 17:07:35 -0700

    Add files via upload

---
+++ CMPUT301F24mikasa/app/src/main/java/com/example/cmput301f24mikasa/EventsJoinedActivity.java
@@ -0,0 +1,9 @@
+package com.example.cmput301f24mikasa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EventsJoinedActivity extends AppCompatActivity {
+
+    // find device ID for current user
+    // fetch any eventID's associated with this user's device ID
+}
