3b6f70a295c96b50768184914db133be119c979f
aPradhan22 <aapradha@ualberta.ca>
2024-11-29 14:53:45 -0700

    Added javadocs for WaitingListArrayAdapter

--- CMPUT301F24mikasa/app/src/main/java/com/example/cmput301f24mikasa/WaitingListArrayAdapter.java
+++ CMPUT301F24mikasa/app/src/main/java/com/example/cmput301f24mikasa/WaitingListArrayAdapter.java
@@ -16,12 +16,23 @@ import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.List;
 
+/**
+ * WaitingListArrayAdapter for displaying a list of user profiles on the waiting list of an event.
+ * Provides functionality to remove users from the waiting list.
+ */
 public class WaitingListArrayAdapter extends android.widget.ArrayAdapter<UserProfile> {
     private Context context;
     private List<UserProfile> userProfiles;
     private String eventID;
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
 
+    /**
+     * Constructor for initializing the adapter.
+     *
+     * @param context      The activity context to update UI elements.
+     * @param userProfiles List of user profiles on the waiting list.
+     * @param eventID      The unique ID for each event.
+     */
     public WaitingListArrayAdapter(@NonNull Context context, @NonNull List<UserProfile> userProfiles, String eventID) {
         super(context, 0, userProfiles);
         this.context = context;
@@ -29,6 +40,14 @@ public class WaitingListArrayAdapter extends android.widget.ArrayAdapter<UserPro
         this.eventID = eventID;
     }
 
+    /**
+     * Inflates the view for each user profile in the waiting list.
+     *
+     * @param position    The position of the current user profile in the list.
+     * @param convertView The old view to reuse, if possible
+     * @param parent      The parent that this view will eventually be attached to.
+     * @return  The view for the current user profile in the waiting list.
+     */
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
@@ -57,6 +76,13 @@ public class WaitingListArrayAdapter extends android.widget.ArrayAdapter<UserPro
         return convertView;
     }
 
+    /**
+     * Removes a user from the waiting list, adds them to the cancelled entrants list,
+     * and removes their location information from the event document in Firestore.
+     *
+     * @param deviceID The unique id of the device.
+     * @param position The position of the user in the waiting list.
+     */
     private void removeUserFromWaitingList(String deviceID, int position) {
         db.collection("event").document(eventID)
                 .update(
