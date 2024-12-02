05157799b64f496a8e747fe56d7e3da5d072c45d
adnan-sami-raiyan <adnansamiraiyan2018@gmail.com>
2024-12-02 00:24:34 -0700

    Update ProfilesActivity.java with Settings import

--- CMPUT301F24mikasa/app/src/main/java/com/example/cmput301f24mikasa/ProfilesActivity.java
+++ CMPUT301F24mikasa/app/src/main/java/com/example/cmput301f24mikasa/ProfilesActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.provider.Settings;
 
 import androidx.appcompat.app.AppCompatActivity;
 
